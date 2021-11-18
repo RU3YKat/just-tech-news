886795970c35f9cfa501e93c94c773cdcc1f7052
insomniKat <ru3ykat@gmail.com>
2021-11-18 16:34:48 -0500

    define user CRU D

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-const Sequelize = require('sequelize');
-
 require('dotenv').config();
+
+const Sequelize = require('sequelize');
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
