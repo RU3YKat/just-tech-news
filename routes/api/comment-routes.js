ff19b49ecfbdd057d58b0c7ab6b7d469b716023d
insomniKat <ru3ykat@gmail.com>
2021-11-21 12:18:42 -0500

    refactor

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,45 +2,44 @@ const router = require('express').Router();
 const { Comment } = require('../../models');
 
 router.get('/', (req, res) => {
-    Comment.findAll()
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err);
+  Comment.findAll()
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
 router.post('/', (req, res) => {
   // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-    })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
-
 router.delete('/:id', (req, res) => {
-    Comment.destroy({
+  Comment.destroy({
     where: {
-        id: req.params.id
+      id: req.params.id
     }
-    })
+  })
     .then(dbCommentData => {
-        if (!dbCommentData) {
+      if (!dbCommentData) {
         res.status(404).json({ message: 'No comment found with this id!' });
         return;
-        }
-        res.json(dbCommentData);
+      }
+      res.json(dbCommentData);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
 });
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User, Post, Vote, Comment } = require('../../models');
+const { User, Post, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
@@ -22,21 +22,11 @@ router.get('/:id', (req, res) => {
     include: [
       {
         model: Post,
-        attributes: [
-          'id',
-          'title',
-          'post_url',
-          'created_at'
-        ]
+        attributes: ['id', 'title', 'post_url', 'created_at']
       },
-      // include comment model here
       {
         model: Comment,
-        attributes: [
-          'id',
-          'comment_text',
-          'created_at'
-        ],
+        attributes: ['id', 'comment_text', 'created_at'],
         include: {
           model: Post,
           attributes: ['title']
@@ -44,9 +34,7 @@ router.get('/:id', (req, res) => {
       },
       {
         model: Post,
-        attributes: [
-          'title'
-        ],
+        attributes: ['title'],
         through: Vote,
         as: 'voted_posts'
       }
