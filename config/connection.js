9bec5c1020467f31a8841b2dadd56aaadff00b2b
insomniKat <ru3ykat@gmail.com>
2021-11-24 14:08:04 -0500

    unmerged file

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+<<<<<<< .merge_file_a10028
 
 require('dotenv').config();
 
@@ -10,5 +11,21 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
+=======
+
+require('dotenv').config();
+
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
+>>>>>>> .merge_file_a16224
 
 module.exports = sequelize;
