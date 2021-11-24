e24c2ff1607cf4cfa09a3c6af0be18f718ee48ff
insomniKat <ru3ykat@gmail.com>
2021-11-24 15:58:51 -0500

    merge conflicts resolved

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 const Sequelize = require('sequelize');
-<<<<<<< .merge_file_a10028
 
 require('dotenv').config();
 
@@ -11,21 +10,5 @@ const sequelize = process.env.JAWSDB_URL
       dialect: 'mysql',
       port: 3306
     });
-=======
-
-require('dotenv').config();
-
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  });
-}
->>>>>>> .merge_file_a16224
 
 module.exports = sequelize;
